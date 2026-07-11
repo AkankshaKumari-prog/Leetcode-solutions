f8dd9a0f76ea5885717a59f0fc95296910eacc5e
Akanksha Kumari <akankshakumariii02@gmail.com>
2026-07-11 15:16:14 +0530

    [88] [Time Beats: 4.11%] [Memory Beats: 15.60%] - Leet2Hub

--- 01-Arrays-and-Hashing/88. Merge Sorted Array/88. Merge Sorted Array.java
+++ 01-Arrays-and-Hashing/88. Merge Sorted Array/88. Merge Sorted Array.java
@@ -1,26 +1,9 @@
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        int[] ans = new int[m + n];
-
-        while(i < m && j < n){
-            if(nums1[i] <= nums2[j]){
-                ans[k++] = nums1[i++];
-            }
-            else{
-                ans[k++] = nums2[j++];
-            }
-        }
-        while(i < m){
-            ans[k++] = nums1[i++];
-        }
-        while(j < n){
-            ans[k++] = nums2[j++];
-        }
-        for(int x=0; x<m+n; x++){
-            nums1[x] = ans[x];
-        }
-    }
-}
+1class Solution {
+2    public void merge(int[] nums1, int m, int[] nums2, int n) {
+3        int j=0;
+4        for(int i=m;i<nums1.length;i++){
+5            nums1[i]=nums2[j++];
+6        }
+7        Arrays.sort(nums1);
+8    }
+9}
